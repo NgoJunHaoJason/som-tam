199e8f1b426e76511e9ff0145a99f6110b0f84d0
Ngo Jun Hao Jason <njhjason@protonmail.com>
2024-10-09 19:06:52 +1300

    Position navbar over banner

--- scripts.js
+++ scripts.js
@@ -1,6 +1,26 @@
 const main = () => {
+  enableNavBar();
   enableNavButton();
   enableMenuButtons();
+};
+
+const enableNavBar = () => {
+  window.onscroll = onScroll;
+};
+
+const onScroll = () => {
+  const banner = document.getElementById("banner");
+  const bannerHeight = banner.clientHeight;
+
+  const navBar = document.getElementById("navbar");
+
+  if (window.scrollY > bannerHeight - 64) {
+    navBar.style.backgroundColor = "var(--secondary-color)";
+    navBar.style.boxShadow = "0px 4px var(--shadow-color)";
+  } else {
+    navBar.style.backgroundColor = "transparent";
+    navBar.style.boxShadow = "none";
+  }
 };
 
 const enableNavButton = () => {
@@ -9,9 +29,13 @@ const enableNavButton = () => {
 };
 
 const toggleNavItems = () => {
+  const navBar = document.getElementById("navbar");
+  navBar.style.backgroundColor = "var(--secondary-color)";
+  navBar.style.boxShadow = "0px 4px var(--shadow-color)";
+
   const navItems = document.getElementById("nav-items");
   navItems.style.display = navItems.style.display === "none" ? "flex" : "none";
-}
+};
 
 const enableMenuButtons = () => {
   const daytimeMenuButton = document.getElementById("daytime-menu-button");
